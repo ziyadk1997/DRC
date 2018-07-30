20bf6433d2c25be217083f74a691f587bcbca023
yacout96 <36423481+yacout96@users.noreply.github.com>
2018-07-30 15:01:49 +0200

    fix to run again

--- src/pages/annual-request/annual-request.ts
+++ src/pages/annual-request/annual-request.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
+/**
+ * Generated class for the AnnualRequestPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
 
 @Component({
   selector: 'page-annual-request',
