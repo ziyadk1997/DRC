d55fe7797e2bef0d68b1aa2664054d14ac02fe4c
yacout96 <36423481+yacout96@users.noreply.github.com>
2018-08-01 11:45:02 +0200

    apply

--- src/pages/apply-leave/apply-leave.ts
+++ src/pages/apply-leave/apply-leave.ts
@@ -2,41 +2,45 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { LeaveTrackerPage } from '../LeaveTracker/LeaveTracker';
 import { WelcomePage } from '../welcome/welcome';
+import { SwitchView } from '../../../node_modules/@angular/common/src/directives/ng_switch';
 
 @Component({
   selector: 'page-apply-leave',
   templateUrl: 'apply-leave.html'
 })
 export class ApplyLeavePage {
-  MyDataFrom:any;
-  MyDataTo:any;
-  Request:any;
-  HideFlag:boolean = false;
+  MyDataFrom: any;
+  MyDataTo: any;
+  Request: any;
+  HideFlag: boolean = false;
+  selected: number;
   constructor(public navCtrl: NavController) {
-    
+
   }
-  GoBack(){
+  GoBack() {
 
     this.navCtrl.pop();
   }
 
-  OnChange(){
+  OnChange() {
     this.HideFlag = false;
   }
-  Display(){
+  Display() {
     this.HideFlag = true;
   }
-  GoToAnnualRequest(){
-    if(this.Request == "Sick" || this.Request == "Annual" || this.Request == "Casual"){
+  GoToAnnualRequest() {
+    if (this.Request == "Sick" || this.Request == "Annual" || this.Request == "Casual") {
       this.navCtrl.push(LeaveTrackerPage)
     }
   }
-  Glow(){
+  Glow(id) {
     console.log("glowed");
+    this.selected = id;
+
   }
-  openHomepage(){
+  openHomepage() {
     this.navCtrl.popToRoot();
   }
-  
- 
+
+
 }
