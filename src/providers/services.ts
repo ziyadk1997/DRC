44488bf16e83ba2536fbe7f1f564f11c0e390d2c
omarrifky <omarkhaledrifky@hotmail.com>
2018-08-02 12:33:40 +0200

    Added services

--- src/providers/services.ts
+++ src/providers/services.ts
@@ -36,4 +36,56 @@ export class ServicesProvider {
         });
     });
   }
+
+  ApplyAnnualandCasualLeaveRequest(username, from , to,comment, timeinday) {
+    var request = JSON.stringify
+      ({
+        "username": username,
+        "From": from,
+        "To" : to,
+        "Comment" : comment,
+        "Timeinday" : timeinday
+
+      });
+
+    return new Promise((resolve, reject) => {
+      this.http.post(config.apiUrl + "ApplyAnnualandCasualLeaveRequest", request).timeout(config.getTimeout).subscribe(res => {
+        try {
+          resolve(res);
+        }
+        catch (e) {
+          reject(false);
+        }
+      },
+        error => {
+          reject(error);
+        });
+    });
+  }
+ 
+  ApproveCasualLeaveRequest(username, from , to,comment, MedicalUrl) {
+    var request = JSON.stringify
+      ({
+        "username": username,
+        "From": from,
+        "To" : to,
+        "Comment" : comment,
+        "MedicalUrl" : MedicalUrl
+
+      });
+
+    return new Promise((resolve, reject) => {
+      this.http.post(config.apiUrl + "ApplySickLeaveRequest", request).timeout(config.getTimeout).subscribe(res => {
+        try {
+          resolve(res);
+        }
+        catch (e) {
+          reject(false);
+        }
+      },
+        error => {
+          reject(error);
+        });
+    });
+  }
 }
