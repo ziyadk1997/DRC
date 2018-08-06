3387646078b1c663e414732b455fe19e65c35539
omarrifky <omarkhaledrifky@hotmail.com>
2018-08-06 12:19:51 +0200

    mj

--- src/pages/RequestInfoPage/RequestInfoPage.ts
+++ src/pages/RequestInfoPage/RequestInfoPage.ts
@@ -11,22 +11,7 @@ export class RequestInfoPage {
   constructor(public navCtrl: NavController, private RequestBE: ServicesProvider) {
   }
   GoToRequestsPage(){
-    var username = localStorage.getItem("username");
-    this.RequestBE.ViewMyReviewedRequests(username).then((res: any) => {
-      console.log(res)
-      if(res!=null){
-      var length = (res.length) / 4;
-      console.log(length);
-      for (let i = 0; i < length; i++) {
-        var request = {
-          Type: res[4 * i],
-          Date: res[4 * i + 1],
-          RUsername: res[4 * i + 2],
-          Status: res[4 * i + 3]
-        }
-        this.requests[i] = request
-      }
-    }});
+    
       this.navCtrl.pop();
   }
 
