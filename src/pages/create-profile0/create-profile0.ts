134b48c83f039b70acc5e0527ab64cd7dc99e41a
mohanadbelal <mohanad.belal98@gmail.com>
2018-08-07 11:00:16 +0200

    Request

--- src/pages/create-profile0/create-profile0.ts
+++ src/pages/create-profile0/create-profile0.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CreateProfile2 } from '../create-profile2/create-profile2';
 
 @Component({
   selector: 'page-create-profile0',
@@ -8,6 +9,9 @@ import { NavController } from 'ionic-angular';
 export class CreateProfilePage {
 
   constructor(public navCtrl: NavController) {
+  }
+  GoToProfile2(){
+    this.navCtrl.push(CreateProfile2);
   }
   
 }
