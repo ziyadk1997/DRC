8d1c4570e4127d61578fbc9d3dfe9971b8e5d24f
mohamedelgabry <mohamedelgabry97@gmail.com>
2018-08-09 16:19:21 +0200

    create

--- src/pages/create-profile0/create-profile0.ts
+++ src/pages/create-profile0/create-profile0.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { CreateProfile2 } from '../create-profile2/create-profile2';
-import { CreateProfile1Page } from '../create-profile/create-profile';
 
 @Component({
   selector: 'page-create-profile0',
@@ -9,10 +8,131 @@ import { CreateProfile1Page } from '../create-profile/create-profile';
 })
 export class CreateProfilePage {
 
+
+  FullName: string;
+  Nationality: String;
+  Address: String;
+  MobileNumber: String;
+  Email: String;
+  newUserInfo = new employer();;
+
+  MaleCheckBox: boolean = false;
+  FemaleCheckBox: boolean = false;
+  SingleCheckBox: boolean = false;
+  MarriedCheckBox: boolean = false;
+  NotApplicableCheckBox: boolean = false;
+  ExampledCheckBox: boolean = false;
+  CompletedCheckBox: boolean = false;
+  PostponedCheckBox: boolean = false;
+
+
+
   constructor(public navCtrl: NavController) {
+
+
+
   }
-  GoToProfile2(){
-    this.navCtrl.push(CreateProfile1Page);
+
+  MaleCheckBoxChanged() {
+
+    if (this.MaleCheckBox == true) {
+      this.newUserInfo.Gender = "male";
+      this.FemaleCheckBox = false;
+      console.log(this.newUserInfo.Gender);
+    }
   }
+  FemaleCheckBoxChanged() {
+
+    if (this.FemaleCheckBox == true) {
+      this.newUserInfo.Gender = "Female"
+      this.MaleCheckBox = false;
+      console.log(this.newUserInfo.Gender);
+
+    }
+  }
+
+  SingleCheckBoxChanged() {
+    if (this.SingleCheckBox == true) {
+      this.newUserInfo.SocialStatus = "Single";
+      this.MarriedCheckBox = false
+      console.log(this.newUserInfo.SocialStatus);
+    }
+
+  }
+
+  MarriedCheckBoxChanged() {
+    if (this.MarriedCheckBox == true) {
+      this.newUserInfo.SocialStatus = "Married";
+      this.SingleCheckBox = false;
+      console.log(this.newUserInfo.SocialStatus);
+    }
+  }
+  NotApplicableCheckBoxChanged() {
+    if (this.NotApplicableCheckBox == true) {
+      this.newUserInfo.MilitaryServices = "Not Applicable";
+      this.ExampledCheckBox = false;
+      this.CompletedCheckBox = false;
+      this.PostponedCheckBox = false;
+      console.log(this.newUserInfo.MilitaryServices);
+    }
+  }
+
+  ExampledChanged() {
+    if (this.ExampledCheckBox == true) {
+      this.newUserInfo.MilitaryServices = "Exampled";
+      this.NotApplicableCheckBox = false;
+      this.CompletedCheckBox = false;
+      this.PostponedCheckBox = false;
+      console.log(this.newUserInfo.MilitaryServices);
+
+    }
+  }
+  CompletedCheckBoxChanged() {
+    if (this.CompletedCheckBox == true) {
+      this.newUserInfo.MilitaryServices = "Completed";
+      this.NotApplicableCheckBox = false;
+      this.ExampledCheckBox = false;
+      this.PostponedCheckBox = false;
+      console.log(this.newUserInfo.MilitaryServices);
+
+    }
+  }
+  PostponedCheckBoxChanged() {
+    if (this.PostponedCheckBox == true) {
+      this.newUserInfo.MilitaryServices = "Postponed";
+      this.NotApplicableCheckBox = false;
+      this.ExampledCheckBox = false;
+      this.CompletedCheckBox = false;
+      console.log(this.newUserInfo.MilitaryServices);
+
+    }
+  }
+
+  GoToProfile2() {
+  //  this.newUserInfo = new employer();
+    
+    this.newUserInfo.FullName=this.FullName;
+    this.newUserInfo.Nationality=this.Nationality;
+    this.newUserInfo.Address=this.Address;
+    this.newUserInfo.MobileNumber=this.MobileNumber;
+    this.newUserInfo.Email=this.Email;
+    console.log(this.newUserInfo);
+
+    this.navCtrl.push(CreateProfile2, this.newUserInfo);
+  }
+
+}
+
+
+export class employer {
+  FullName: string;
+  Gender: String;
+  Nationality: String;
+  Address: String;
+  MobileNumber: String;
+  Email: String;
+  SocialStatus: String;
+  MilitaryServices: String;
+  JobTitle:string;
   
 }
--- src/pages/create-profile2/create-profile2.ts
+++ src/pages/create-profile2/create-profile2.ts
@@ -2,18 +2,29 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { CreateProfile1Page } from '../create-profile/create-profile';
 import {CreateProfile3} from '../create-profile3/create-profile3';
+import { NavParams } from 'ionic-angular';
+import { employer } from '../create-profile0/create-profile0';
+
 
 @Component({
   selector: 'page-create-profile2',
   templateUrl: 'create-profile2.html'
 })
 export class CreateProfile2 {
-    
+  JobTitle:any;
+  newUserInfo=new employer();
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController , public navParams: NavParams) {
+    this.newUserInfo=this.navParams.data;
+    console.log(this.newUserInfo);
   }
   
 
+  Training(){
+    this.JobTitle="Training";
+
+    console.log(this.newUserInfo.JobTitle);
+  }
   GoToCreate3(){
     this.navCtrl.push(CreateProfile3);
   }
